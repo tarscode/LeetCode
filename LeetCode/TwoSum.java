2890df493f9a62dc8ea7485d86515598c50dd054
tarsdroid <tarsdroid@hotmail.com>
2016-08-19 12:11:24 +0800

    一趟查询,最优解,时间复杂度O(n)

--- LeetCode/TwoSum.java
+++ LeetCode/TwoSum.java
@@ -1,29 +1,21 @@
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by liuyang on 16/8/18.
- * 备注:两趟查询,时间复杂度O(n)
+ * 备注:一趟查询,最优解,时间复杂度O(n)
  */
 public class TwoSum {
     public int[] twoSum(int[] nums, int target) {
-        int n = nums.length;
-        int[] num = new int[2];
-        int value = 0;
-        HashMap map = new HashMap();
-        for (int i = 0; i < n; i++) {
+        Map<Integer, Integer> map = new HashMap();
+        for (int i = 0; i < nums.length; i++) {
+            int value = target - nums[i];
+            value = target-nums[i];
+            if (map.containsKey(value)) {
+                return new int[]{map.get(value), i};
+            }
             map.put(nums[i], i);
         }
-        for (int i = 0; i < n; i++) {
-            value = target - nums[i];
-            if (map.containsKey(value)) {
-                int j = Integer.valueOf(map.get(value).toString()).intValue();
-                if (j != i) {
-                    num[0] = i;
-                    num[1] = j;
-                    break;
-                }
-            }
-        }
-        return num;
+        throw new IllegalArgumentException("No two sum solution");
     }
 }
