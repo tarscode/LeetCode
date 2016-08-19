d1bcf6c8774a5d8ce962b7b0b4eec68471c91aee
tarsdroid <tarsdroid@hotmail.com>
2016-08-19 11:23:39 +0800

    两趟查询,时间复杂度O(n)

--- LeetCode/TwoSum.java
+++ LeetCode/TwoSum.java
@@ -1,14 +1,23 @@
+import java.util.HashMap;
+
 /**
  * Created by liuyang on 16/8/18.
- * 备注:暴力算法
+ * 备注:两趟查询,时间复杂度O(n)
  */
 public class TwoSum {
     public int[] twoSum(int[] nums, int target) {
         int n = nums.length;
         int[] num = new int[2];
+        int value = 0;
+        HashMap map = new HashMap();
         for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (nums[i] + nums[j] == target) {
+            map.put(nums[i], i);
+        }
+        for (int i = 0; i < n; i++) {
+            value = target - nums[i];
+            if (map.containsKey(value)) {
+                int j = Integer.valueOf(map.get(value).toString()).intValue();
+                if (j != i) {
                     num[0] = i;
                     num[1] = j;
                     break;
