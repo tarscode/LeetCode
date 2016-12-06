bb3e0986f9f16a290b92bbc5cacc890fded678e8
tarsdroid <tarsdroid@hotmail.com>
2016-12-06 19:07:23 +0800

    Remove Duplicates from Sorted List2(移除有序链表中的重复元素2)

--- LeetCode/RemoveDuplicatesfromSortedList2.java
+++ LeetCode/RemoveDuplicatesfromSortedList2.java
@@ -4,7 +4,7 @@
  * 【时间】: 16/12/2 下午9:05
  * 【题目】: Remove Duplicates from Sorted List2(移除有序链表中的重复元素2)
  * 【内容】: 给定一个有序链表,删除所有有重复值的节点,只留下没有重复值的节点。例如：1->2->3->3->4->4->5,返回：1->2->5.
- * 【版本】: V1.0
+ * 【版本】: V2.0
  * 【运行时间】:
  * 【时间复杂度】:
  * 【空间复杂度】:
@@ -12,35 +12,35 @@
  * 【思路】:
  */
 public class RemoveDuplicatesfromSortedList2 {
+    //方法一：4个变量
     public ListNode deleteDuplicates(ListNode head) {
         if (head == null) {
             return head;
         }
-        ListNode head2 = new ListNode(-100);
+        //创建虚拟头结点head2，处理原头节点非法的情况
+        ListNode head2 = new ListNode(head.val-1);
         head2.next = head;
         ListNode node = head2;
         ListNode pre = head2;
         ListNode cur = pre.next;
         while (cur != null) {
-            if (cur.next == null) {
-                if (cur.val == pre.val) {
-                    node.next = null;
-                } else {
-                    node.next = cur;
-                    node = node.next;
-                }
-            } else {
-                if (cur.val != pre.val && cur.val != cur.next.val) {
-                    node.next = cur;
-                    node = node.next;
-                } else {
-                }
+            //cur为最后一个节点时，若cur与pre值相等，则cur无效
+            if (cur.next == null && cur.val == pre.val){
+                node.next = null;
             }
+            //加入新链表的两种情况:(1)cur与pre、next的值均不相等(2)next为空的情况下，cur与pre不等
+            if ((cur.next != null && cur.val != pre.val && cur.val != cur.next.val)||(cur.next == null && cur.val != pre.val)) {
+                node.next = cur;
+                node = node.next;
+            }
+            //每次遍历cur、pre依次后移
             pre = pre.next;
             cur = cur.next;
         }
         return head2.next;
     }
+
+    //方法二：3个变量
     /*
     public ListNode deleteDuplicates(ListNode head) {
         if (head == null) {
