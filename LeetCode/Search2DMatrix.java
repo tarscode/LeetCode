39bc4ac0b795941fe96225b479e427dafb09f66a
tarsdroid <tarsdroid@hotmail.com>
2016-12-09 21:31:38 +0800

    Remove Duplicates from Sorted List(移除有序链表中的重复元素)

--- LeetCode/Search2DMatrix.java
+++ LeetCode/Search2DMatrix.java
@@ -4,22 +4,20 @@
  * 【时间】: 16/12/8 下午1:45
  * 【题目】: Search a 2D Matrix(二维矩阵查找)
  * 【内容】: 给定一个矩阵，特点是：每一行从左至右递增，下一行的第一个数大于上一行的最后一个数，在矩阵中查找目标值是否存在
- * 【版本】: V1.0
+ * 【版本】: V1.1
  * 【运行时间】:
  * 【时间复杂度】:
  * 【空间复杂度】:
- * 【备注】:
+ * 【备注】: 精简代码
  * 【思路】:
- *  首先，遍历每一行的最后一个数，直到找到一个数大于target
- *  然后，遍历这个数所在行的每一个数，若有数等于目标值，则返回true，否则返回false
+ * 首先，遍历每一行的最后一个数，直到找到一个数大于target
+ * 然后，遍历这个数所在行的每一个数，若有数等于目标值，则返回true，否则返回false
  */
 public class Search2DMatrix {
     public boolean searchMatrix(int[][] matrix, int target) {
         for (int i = 0; i < matrix.length; i++) {
             int col = matrix[i].length;
-            if (matrix[i][col - 1] < target) {
-                continue;
-            } else {
+            if (matrix[i][col - 1] >= target) {
                 for (int j = col - 1; j >= 0; j--) {
                     if (matrix[i][j] == target) return true;
                 }
