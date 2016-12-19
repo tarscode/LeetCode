63c157b8db0f4e2638d005d1108c8a002739afe4
tarsdroid <tarsdroid@hotmail.com>
2016-12-19 21:51:26 +0800

    Path Sum(路径和)V2.0

--- LeetCode/PathSum.java
+++ LeetCode/PathSum.java
@@ -3,31 +3,19 @@
  * 【作者】: liuyang
  * 【时间】: 16/12/19 下午9:04
  * 【题目】: Path Sum(路径和)
- * 【内容】: 给定一个二叉树和一个目标值,判断是否存在根节点至叶子节点的路径和等于目标值
- * 【版本】: V1.0
+ * 【内容】: 给定一个二叉树和一个目标值,判断是否存在根节点至叶子节点的路径和
+ * 【版本】: V2.0
  * 【运行时间】:
  * 【时间复杂度】:
  * 【空间复杂度】:
- * 【备注】:
+ * 【备注】: 精简代码,只需要一个函数
  * 【思路】:
  */
 public class PathSum {
     public boolean hasPathSum(TreeNode root, int sum) {
-        if (hasPathSum(root, sum, 0)) {
-            return true;
-        }
-        return false;
-    }
-
-    public boolean hasPathSum(TreeNode node, int sum, int cur) {
-        if (node == null) {
-            return false;
-        }
-        cur += node.val;
-        if (node.left == null && node.right == null && cur == sum) {
-            return true;
-        }
-        return hasPathSum(node.left, sum, cur) || hasPathSum(node.right, sum, cur);
+        if (root == null) return false;
+        if (root.left == null && root.right == null && sum == root.val) return true;
+        return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
     }
 
     public static void main(String[] args) {
