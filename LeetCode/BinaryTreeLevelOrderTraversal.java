9da9cd481591418b06bee41f1ec66cf8ca71e328
tarsdroid <tarsdroid@hotmail.com>
2016-12-21 20:18:54 +0800

    Binary Tree Level Order Traversal(二叉树层次遍历)

--- LeetCode/BinaryTreeLevelOrderTraversal.java
+++ LeetCode/BinaryTreeLevelOrderTraversal.java
@@ -1,7 +1,6 @@
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Queue;
 
 /**
  * 【工程】: LeetCode 包名: PACKAGE_NAME 类名: BinaryTreeLevelOrderTraversal
@@ -17,6 +16,34 @@ import java.util.Queue;
  * 【思路】:
  */
 public class BinaryTreeLevelOrderTraversal {
+    //记录本层有多少个法
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        LinkedList<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            List<Integer> level = new ArrayList<>();
+            int num = queue.size();
+            //每一次遍历本层的数目
+            for (int i = 0; i < num; i++) {
+                TreeNode cur = queue.poll();
+                level.add(cur.val);
+                if (cur.left != null) {
+                    queue.add(cur.left);
+                }
+                if (cur.right != null) {
+                    queue.add(cur.right);
+                }
+            }
+            res.add(level);
+        }
+        return res;
+    }
+    //记录上一层最后一个值方法
+    /*
     public List<List<Integer>> levelOrder(TreeNode root) {
         List<List<Integer>> res = new ArrayList<>();
         if(root==null){
@@ -46,6 +73,7 @@ public class BinaryTreeLevelOrderTraversal {
         }
         return res;
     }
+    */
 
     public static void main(String[] args) {
         TreeNode root = new TreeNode(3);
